1b575f773b72bdfd62acd0455a1f535646f843a1
mistrivishal <mistri.vishal95@gmail.com>
2022-06-01 19:35:15 +0530

    Day10

---
+++ Java Day 10/src/com/masai/Q4/Area.java
@@ -0,0 +1,24 @@
+package com.masai.Q4;
+
+public class Area extends Shape{
+
+	@Override
+	public int rectangleArea(int length, int breadth) {
+		int rectangleArea = length * breadth;
+		return rectangleArea;
+	}
+
+	@Override
+	public int squareArea(int side) {
+		int squareArea = side*side;
+		return squareArea;
+	}
+
+	@Override
+	public int circleArea(int radius) {
+		int circleArea = (int) (Math.PI*(radius*radius));
+		return circleArea;
+	}
+	
+	
+}
