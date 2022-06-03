3eebe8f49623e3278d90edfc67cd66f159b40401
mistrivishal <mistri.vishal95@gmail.com>
2022-06-03 09:44:49 +0530

    Day 11Q2

--- Java Day 11/src/com/masai/Q2/Mobile.java
+++ Java Day 11/src/com/masai/Q2/Mobile.java
@@ -6,25 +6,17 @@ public class Mobile {
 	
 	static String[] outdatedModels = {"note4","note5","note6","note7"};
 	
-	public static void searchOutdatedModel(String []...modelNo){
+	public static void searchOutdatedModel(String name, String modelNo){
 		
 		if(modelNo!=null) {
 			
 			for (String outdatedModel : outdatedModels) {
-				for(String[] x : modelNo) {
-					if(x[1].equalsIgnoreCase(outdatedModel)) {
-						
-						System.out.println(x[0] + " " + x[1] + "_OUTDATED");
-						System.out.println("\n********************************\n");
-						break;
-						
-					}else {
-						
-						System.out.println(x[0] + " " + x[1] + "_NOT_OUTDATED");
-						System.out.println("\n********************************\n");
-						break;
-						
-					}
+				if(modelNo.equalsIgnoreCase(outdatedModel)) {
+					System.out.println(name + " " + modelNo + "_OUTDATED");
+					break;
+				}else {
+					System.out.println(name + " " + modelNo + "_NOT_OUTDATED");
+					break;
 				}
 			}
 		}
@@ -66,8 +58,11 @@ public class Mobile {
 			System.out.println("\n********************************\n");
 		}
 		
-		//Outdated mobile search method call with var args:-
-		
-		Mobile.searchOutdatedModel(models);
+		for(int i=0;i<models.length;i++) {
+//			System.out.println(models[i][0]);
+//			System.out.println(models[i][1]);
+			Mobile.searchOutdatedModel(models[i][0],models[i][1]);
+			System.out.println("\n********************************\n");
+		}
 	}
 }
