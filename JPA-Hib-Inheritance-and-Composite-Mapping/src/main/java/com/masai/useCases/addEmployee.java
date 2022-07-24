444575b4f190e6032eb206f04198ebd8bb25fa4d
mistrivishal <mistri.vishal95@gmail.com>
2022-07-24 13:17:32 +0530

    Inheritance_Composite_Mapping

--- JPA-Hib-Inheritance-and-Composite-Mapping/src/main/java/com/masai/useCases/addEmployee.java
+++ JPA-Hib-Inheritance-and-Composite-Mapping/src/main/java/com/masai/useCases/addEmployee.java
@@ -9,68 +9,68 @@ import com.masai.entityClasses.Employee;
 import com.masai.utilEntity.UtilEntity;
 
 public class addEmployee {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		EntityManager em =  UtilEntity.getEntityManager();
-		
+
+		EntityManager em = UtilEntity.getEntityManager();
+
 		System.out.println("Insert 4 Employee record ");
 		boolean yes = true;
-		
-		while(yes) {
-			
+
+		while (yes) {
+
 			Employee e = new Employee();
-			
+
 			System.out.println("Enter employe name:");
-			String name =  sc.next();
+			String name = sc.next();
 			e.setName(name);
-			
+
 			System.out.println("Enter employee salary;");
 			int salary = sc.nextInt();
 			e.setSalary(salary);
-			
+
 			String type = "Home";
-			
+
 			int i = 0;
-			
-			while(i<2) {
+
+			while (i < 2) {
 				System.out.println("Enter " + type + " address of employee:");
-				
+
 				System.out.println("Enter state:");
 				String state = sc.next();
-				
+
 				System.out.println("Enter city:");
 				String city = sc.next();
-				
+
 				System.out.println("Enter pincode:");
 				String pincode = sc.next();
-				
-				if(type.equalsIgnoreCase("Home")) {
+
+				if (type.equalsIgnoreCase("Home")) {
 					e.setHomeAddress(new Address(state, city, pincode));
-				}else {
+				} else {
 					e.setOfficeAddress(new Address(state, city, pincode));
 				}
-				
+
 				type = "Office";
 				i++;
 			}
-			
+
 			em.getTransaction().begin();
-			
+
 			em.persist(e);
-			
+
 			em.getTransaction().commit();
-			
+
 			System.out.println("Enter Y to add another employee/ Else press any to exit:");
-			if(!sc.next().equalsIgnoreCase("y")) {
+			if (!sc.next().equalsIgnoreCase("y")) {
 				break;
 			}
 		}
-		
-		System.out.println("Data inserted succeefully...");
-		
+
+		System.out.println("Employee Data inserted succeefully...");
+
 		em.close();
 	}
 }
